49b0b9b1b5ede1bfeaccf22f6a5b7676964b9bc9
Riccardo <riccardo@Lauras-iMac.local>
2017-06-30 15:13:17 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+//Libs
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//Components
+import Scoreboard from './components/Scoreboard';
+
+//css
+import './css/style.css';
+
+
+
+ReactDOM.render(<Scoreboard />, document.getElementById('container'));
